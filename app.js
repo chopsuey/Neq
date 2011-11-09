64eb29790f46fac63a10aaa6b67bc9a3d794f364
dst <neq.healthcare@googlemail.com>
2011-11-09 20:48:12 +0100

    update

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 Ext.application({
-    name: 'NEQ Healthcare',
+    name: 'Sencha',
 
     launch: function() {
         Ext.create("Ext.TabPanel", {
             fullscreen: true,
+            tabBarPosition: 'bottom',
+
             items: [
                 {
-                    title: 'Home',
+                    title: 'Neq Healthcare',
                     iconCls: 'home',
-                    html: 'Welcome'
+                    cls: 'home',
+                    html: [
+                        '<img width="65%" src="neq.png" />',
+                        '<h1>Welcome NEQ Healthcare/h1>'
+                    ].join("")
+                },
+                {
+                    xtype: 'list',
+                    title: 'Blog',
+                    iconCls: 'star',
+
+                    itemTpl: '{title}',
+                    store: {
+                        fields: ['title', 'url'],
+                        data: [
+                            {title: 'Ext Scheduler 2.0', url: 'ext-scheduler-2-0-upgrading-to-ext-js-4'},
+                            {title: 'Previewing Sencha Touch 2', url: 'sencha-touch-2-what-to-expect'},
+                            {title: 'Sencha Con 2011', url: 'senchacon-2011-now-packed-with-more-goodness'},
+                            {title: 'Documentation in Ext JS 4', url: 'new-ext-js-4-documentation-center'}
+                        ]
+                    }
                 }
             ]
-        });
+        }).setActiveItem(1);
     }
 });
+
